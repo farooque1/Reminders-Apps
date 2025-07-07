adf2974b44f7714590cbf0487fa87f9370401847
Mohammed Farooque Shaikh <41079646+farooque1@users.noreply.github.com>
2025-07-07 17:12:58 +0530

    Update settings.ts

--- src/Auth/settings.ts
+++ src/Auth/settings.ts
@@ -6,7 +6,7 @@ interface Settings {
   const settings: Settings = {
     // API_BASE_URL: "https://salestvsapp.truetab.ai/api/v2",
     // API_BASE_URL: "https://job-backend-production-bc04.up.railway.app/api",
-    API_BASE_URL: "  http://localhost:5000/api",
+    API_BASE_URL: "  https://reminders-apps-backend.vercel.app/api",
     API_BASE_URL_DOWNLOAD: "https://server-g7aa.onrender.com/api/",
   };
   export default settings;
